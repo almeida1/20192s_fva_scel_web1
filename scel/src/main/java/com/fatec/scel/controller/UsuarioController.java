da079b05409272c879a27ff8b016d77bc01dd73a
professor <professor@L2MP.IPIRANGA.FATEC>
2019-10-03 15:55:36 -0300

    Inclusao de usuario - ok

--- scel/src/main/java/com/fatec/scel/controller/UsuarioController.java
+++ scel/src/main/java/com/fatec/scel/controller/UsuarioController.java
@@ -5,12 +5,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-
+import com.fatec.scel.model.Livro;
 import com.fatec.scel.model.Usuario;
 import com.fatec.scel.model.UsuarioRepository;
 
@@ -34,6 +35,26 @@ public class UsuarioController {
 
 		return mv;
 	}
+	@GetMapping("/edit/{ra}") // diz ao metodo que ira responder a uma requisicao do tipo get
+	public ModelAndView mostraFormAdd(@PathVariable("ra") String ra) {
+		ModelAndView modelAndView = new ModelAndView("AtualizaUsuario");
+
+		modelAndView.addObject("usuario", repository.findByRa(ra)); // o repositorio e injetado no controller
+
+		return modelAndView; // addObject adiciona objetos para view
+
+	}
+
+	@GetMapping("/delete/{id}")
+	public ModelAndView delete(@PathVariable("id") Long id) {
+
+		repository.deleteById(id);
+		ModelAndView modelAndView = new ModelAndView("ConsultarUsuario");
+		modelAndView.addObject("usuarios", repository.findAll());
+		return modelAndView;
+
+	}
+
 	@PostMapping("/save")
 	public ModelAndView save(@Valid Usuario usuario, BindingResult result) {
 		ModelAndView modelAndView = new ModelAndView("ConsultarUsuario");
@@ -55,5 +76,21 @@ public class UsuarioController {
 			System.out.println("erro ===> " +e.getMessage());
 			return modelAndView; // captura o erro mas nao informa o motivo.
 		}
+	}
+	@PostMapping("/update/{id}")
+	public ModelAndView atualiza(@PathVariable("id") Long id, @Valid Usuario usuario, BindingResult result) {
+	
+		if (result.hasErrors()) {
+			usuario.setId(id);
+			return new ModelAndView("AtualizaUsuario");
+		}
+		Usuario umUsuario = repository.findById(id).get();
+		umUsuario.setRa(usuario.getRa());
+		umUsuario.setNome(usuario.getNome());
+		umUsuario.setEmail(usuario.getEmail());
+		repository.save(umUsuario);
+		ModelAndView modelAndView = new ModelAndView("ConsultarUsuario");
+		modelAndView.addObject("usuarios", repository.findAll());
+		return modelAndView;
 	}
 }
