f08f0b27f6f636beaad9333d120a233bcafd6da5
HP <HP@DV6-6170>
2019-11-07 11:45:00 -0200

    Refatoraçao da classe usuario

--- scel/src/main/java/com/fatec/scel/controller/UsuarioController.java
+++ scel/src/main/java/com/fatec/scel/controller/UsuarioController.java
@@ -9,11 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fatec.scel.model.Endereco;
-import com.fatec.scel.model.Livro;
 import com.fatec.scel.model.Usuario;
 import com.fatec.scel.model.UsuarioRepository;
 import com.fatec.scel.model.Servico;
@@ -25,7 +22,6 @@ public class UsuarioController {
 	private UsuarioRepository repository;
 	@Autowired
 	private Servico servico;
-
 	@GetMapping("/consulta")
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ConsultarUsuario");
@@ -34,35 +30,26 @@ public class UsuarioController {
 	}
 	@GetMapping("/cadastrar")
 	public ModelAndView cadastraUsuario(Usuario usuario) {
-
 		ModelAndView mv = new ModelAndView("CadastrarUsuario");
 		mv.addObject("usuario", usuario);
-
 		return mv;
 	}
 	@GetMapping("/edit/{ra}") // diz ao metodo que ira responder a uma requisicao do tipo get
 	public ModelAndView mostraFormAdd(@PathVariable("ra") String ra) {
 		ModelAndView modelAndView = new ModelAndView("AtualizaUsuario");
-
 		modelAndView.addObject("usuario", repository.findByRa(ra)); // o repositorio e injetado no controller
-
 		return modelAndView; // addObject adiciona objetos para view
-
 	}
-
 	@GetMapping("/delete/{id}")
 	public ModelAndView delete(@PathVariable("id") Long id) {
-
 		repository.deleteById(id);
 		ModelAndView modelAndView = new ModelAndView("ConsultarUsuario");
 		modelAndView.addObject("usuarios", repository.findAll());
 		return modelAndView;
-
 	}
 
 	@PostMapping("/save")
 	public ModelAndView save(@Valid Usuario usuario, BindingResult result) {
-		
 		ModelAndView modelAndView = new ModelAndView("ConsultarUsuario");
 		if (result.hasErrors()) {
 			return new ModelAndView("CadastrarUsuario");
--- scel/src/main/java/com/fatec/scel/model/Servico.java
+++ scel/src/main/java/com/fatec/scel/model/Servico.java
@@ -1,50 +1,16 @@
 package com.fatec.scel.model;
 
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import org.joda.time.*;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class Servico {
 	
-   
-	
-	/**
-	 * valida o formato da data
-	 * 
-	 * @param data
-	 *            no formato yyyy/MM/dd
-	 * @return true se a data estiver no formato valido e false para formato
-	 *         invalido
-	 */
-	public boolean validaData(String data) {
-		DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
-		df.setLenient(false); //
-		try {
-			df.parse(data); // data válida
-			return true;
-		} catch (ParseException ex) {
-			return false;
-		}
-	}
-	public String obtemEndereco(String cep) {
+  	public String obtemEndereco(String cep) {
 		RestTemplate template = new RestTemplate();
 		String url = "https://viacep.com.br/ws/{cep}/json/";
 		Endereco endereco = template.getForObject(url,Endereco.class,cep);
 		return endereco.getLogradouro();
 	}
-	public String cadastrarUsuario(Usuario usuario) {
-		RestTemplate template = new RestTemplate();
-		String url = "https://viacep.com.br/ws/{cep}/json/";
-		
-		return null;
-	}
+	
 }
